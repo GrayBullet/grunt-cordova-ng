e9793a51a30775b99405be3ae27b6c021acf68d5
Tomo Masakura <tomo.masakura@gmail.com>
2014-12-22 18:24:51 +0900

    Append copy apk files to dist directory task.

--- tasks/cordova.js
+++ tasks/cordova.js
@@ -1,6 +1,17 @@
 var _ = require('underscore');
 var cordova = require('cordova');
 
+var getTaskName = function (task, command) {
+  'use strict';
+
+  var array = _.clone(task.args);
+  array.unshift(task.name);
+
+  array[1] = command;
+
+  return array.join(':');
+};
+
 var execute = function (command, options, callback) {
   'use strict';
 
@@ -26,12 +37,20 @@ module.exports = function (grunt) {
   var util = require('../lib/util.js');
 
   grunt.registerTask('cordova', 'Run cordova command.', function (command, build) {
+    // 'cordova:package' task is ['cordova:build', 'cordova:package-files']
+    if (command === 'package') {
+      grunt.task.run([
+        getTaskName(this, 'build'),
+        getTaskName(this, 'package-files')
+      ]);
+      return;
+    }
+
     // Show run cordova results.
     cordova.on('results', function (results) {
       grunt.log.writeln(results);
     });
 
-    var done = this.async();
     var args = _.toArray(arguments);
     args.shift();
 
@@ -63,13 +82,18 @@ module.exports = function (grunt) {
 
     grunt.log.debug(JSON.stringify(newOptions));
 
-    execute(command, newOptions, function (error) {
+    if (command === 'package-files') {
+      var packageFiles = (require('../lib/findPackage'))(newOptions);
+      packageFiles(grunt, newOptions);
+    } else {
+      var done = this.async();
 
-      if (error) {
-        grunt.fail.warn(error);
-      }
-
-      done();
-    });
+      execute(command, newOptions, function (error) {
+        if (error) {
+          grunt.fail.warn(error);
+        }
+        done();
+      });
+    }
   });
 };
